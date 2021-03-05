02c8945d387ab067221618cb665a9052e38ace32
pranesh12 <praneshchakama6@gmail.com>
2021-03-05 17:49:53 +0600

    whatver

--- ema-john/src/App.js
+++ ema-john/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Notfound from "./components/NotFound/Notfound";
+import Inventory from "./components/Inventory/Inventory";
+import Review from "./components/Review/Review";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="product/:productKey">
+            <ProductDetails></ProductDetails>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route path="*">
+            <Notfound></Notfound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- ema-john/src/components/Product/Product.js
+++ ema-john/src/components/Product/Product.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import './Product.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Product.css";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Product = (props) => {
-    // console.log(props);
-    const { img, name, seller, price, stock } = props.product;
-    return (
-        <div className="product">
-            <div>
-                <img src={img} alt="" />
-            </div>
-            <div>
-                <h4 className="product-name">{name}</h4>
-                <br />
-                <p><small>by: {seller}</small></p>
-                <p>${price}</p>
-                <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button 
-                    className="main-button" 
-                    onClick={() => props.handleAddProduct(props.product)}
-                    > 
-                        <FontAwesomeIcon icon={faShoppingCart} /> add to cart
-                    </button>
-            </div>
-
-        </div>
-    );
+  // console.log(props);
+  const { img, name, seller, price, stock } = props.product;
+  const productKey = useParams();
+  console.log(productKey);
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <Link to="/product/wa">
+          <h4 className="product-name">{name}</h4>
+        </Link>
+        <br />
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>${price}</p>
+        <p>
+          <small>Only {stock} left in stock - Order soon</small>
+        </p>
+        <button
+          className="main-button"
+          onClick={() => props.handleAddProduct(props.product)}
+        >
+          <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
